8f1360a3d542ca19fe43bed95a371b69ceef798d
Chr1sM1 <mzs0119@qq.com>
2021-06-06 15:42:57 +0800

    TextView实现跑马灯

---
+++ TextViewMarquee/app/src/main/java/com/example/marquee/MyTextView.java
@@ -0,0 +1,29 @@
+package com.example.marquee;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.Nullable;
+
+public class MyTextView extends android.widget.TextView {
+    public MyTextView(Context context) {
+        super(context);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public boolean isFocused() {
+        return true;
+    }
+}
